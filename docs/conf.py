1bc453801e73563a6653aef0a41ec6260c596704
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-05-27 15:29:28 +0100

    change for ome 2020

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2019-2020, Open Microscopy Environment'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.1a1'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
